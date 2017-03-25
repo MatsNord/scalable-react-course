8fdfe031cb1f3303eb49122eaca44a6e5a4fc7c8
Mats Nord <nord.mats@gmail.com>
2017-03-25 09:40:33 +0100

    Module Handling event with Redux-saga done

--- app/containers/LinkListContainer/sagas.js
+++ app/containers/LinkListContainer/sagas.js
@@ -1,8 +1,30 @@
 // import { take, call, put, select } from 'redux-saga/effects';
 
+import { call, put } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { SELECT_TOPIC } from '../NavigationContainer/constants';
+import { requestLinksSucceeded, requestLinksFailed } from './actions';
+
+
+function fetchLinksFromServer(topic) {
+  return fetch(`http://localhost:3000/api/topics/${topic.name}/links`)
+    .then(response => response.json());
+}
+
+function* fetchLinks(action){
+  try {
+    const links = yield call(fetchLinksFromServer, action.topic);
+    // dispatch action to store links
+    yield put(requestLinksSucceeded(links));
+  } catch (e) {
+    yield put(requestLinksFailed(e.mesage));
+    //dispatch action to server error
+  }
+}
+
 // Individual exports for testing
 export function* defaultSaga() {
-  return;
+  yield* takeLatest(SELECT_TOPIC, fetchLinks);
 }
 
 // All sagas to be loaded
