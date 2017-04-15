51e2ad69ebe0fac28cdebed8a711091fd8d6ce3f
Mats Nord <nord.mats@gmail.com>
2017-04-15 18:52:05 +0200

    Presenting validation message on a form

--- app/components/Login/index.js
+++ app/components/Login/index.js
@@ -7,9 +7,41 @@
 import React from 'react';
 
 import styles from './styles.css';
+import validator from 'email-validator';
+import classNames from 'classnames';
 
 class Login extends React.Component { // eslint-disable-line react/prefer-stateless-function
+  // instead of doing this.state in the constructor 
+  state = {};
+  
+  // Binding the field to this component. 
+  // The alternative would be to bind the field with .bind(this);
+  login = () => {
+    const email = this.emailField.value;
+    if (!validator.validate( email )){
+      this.setState({
+        errorText: 'Please provide a valid email',
+      });
+      return;
+    }
+    this.setState({
+      errorText: null,
+    });
+  }
+
+
+  // login() {
+  // use bind on the button
+  //   console.log('login button clicked!', this.emailField.value);
+  // }
   render() {
+    const fieldError = this.state.errorText ? (
+      <div
+        className={styles.errorMessage}
+      >
+        {this.state.errorText}
+      </div>
+    ) : null;
     return (
       <div className={styles.login}>
         <div
@@ -18,11 +50,12 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
           Login with your email
         </div>
         <input
-          className={styles.input}
+          className={classNames( styles.input, {[styles.inputError]:this.state.errorText})}
           placeholder="Your email"
           ref={(f) => { this.emailField = f; }}
           type="text"
         />
+        {fieldError}
         <div
           className={styles.actionContainer}
         >
@@ -33,6 +66,8 @@ class Login extends React.Component { // eslint-disable-line react/prefer-statel
           </div>
           <div
             className={styles.button}
+            //onClick={this.login.bind(this)}
+            onClick={this.login}
           >
             login in
           </div>
